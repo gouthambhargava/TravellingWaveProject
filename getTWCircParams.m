ec6f717e06e2833c328adb7029cf06026039d902
Supratim Ray <supratimray@gmail.com>
2024-03-27 20:33:46 +0530

    Several changes in the way data is displayed

--- getTWCircParams.m
+++ getTWCircParams.m
@@ -1,79 +1,83 @@
-function [outputs] = getTWCircParams(filteredSignal,burstData,timeVals,goodElectrodes,nPerm)
-%% Inputs
-%%Inputs
-% data - filtered single trial data in the electrodes x time points format
-% goodElectrodes - list of good electrodes
+function outputs = getTWCircParams(phiMat,burstMat,timeVals,goodElectrodes,locList,electrodeFraction,nPerm)
+% Inputs
+% phiMat - phases of single trial data in electrodes x time points format
+% burstMat - time series indicting burst times in electrodes x time points format
 % timeVals - vector of time values
-% burstData - time series of burst locations
-%% 
-if nargin<5
-    nPerm = [];
-end
-%% Parameters
-    elecDist = 400*10^-6; %distance between adjacent electrodes in the array in m
-    fs = 1/(timeVals(2)-timeVals(1)); %sampling frequency
-%% filter and extract instant phase of the lfp data
-    phiMat = angle(hilbert(filteredSignal'))';
-    gridLayout = rot90(reshape(1:81,[9,9]),2); %set the grid layout
-    timePoints = length(timeVals);
-    
-    %initialize results
-    pgd = zeros(timePoints,1);
-    direction = zeros(timePoints,1);
-    cluster = zeros(timePoints,1);
-    circVmean = zeros(timePoints,1);
-    sFreq = zeros(timePoints,1);
-    mag = zeros(timePoints,1);
-    
-    if ~isempty(nPerm)
-        pgdPerm = zeros(timePoints,nPerm);
-    end
-    % get location for each electrode for segmentation into clusters 
-    locList = nan(length(goodElectrodes),2);
-    for i = 1:numel(goodElectrodes)
-        [locList(i,1),locList(i,2)] = find(gridLayout==goodElectrodes(i));
-    end
-    locList = cat(2,locList,ones(length(locList),1));
-    clear i
+% goodElectrodes - list of good electrodes
+% locList - array of size electrodes x 2 containing the x and y position of each electrode
+% electrodeFraction - identify as a cluster only when the number of
+% electrodes showing a burst exceeds this fraction. However, when set to
+% zero, all electrodes are chosen for analysis
+% nPerm - when not set to [], TW statistics are computed nPerm times after shuffling the phases.
 
-    % run circ reg after getting significant electrodes from burst
-    % detection
-        for timei = 1:size(phiMat,2)
-            phiGrid = phiMat(:,timei);
-            elecs = find(burstData(:,timei));          
-                   %skip to next time point if cluster has <4 electrodes
-                   if numel(elecs)>3
-                        cluster(timei,1) = numel(elecs);
-                        circularCord = phiGrid(elecs);
-                        mag(timei,:) = sqrt(sum(phiGrid.^2));
-                        circVmean(timei,1) = circ_mean(circularCord);
-                        linearCord = locList(elecs,:,:);
-                        linearCord(:,3,:) = [];
-                    % do regression analysis on the polar and linear coordinates
-                        [direction(timei,1),sFreq(timei,1),~,~,pgd(timei,1)] = circRegMod(circularCord,linearCord);
-                        if ~isempty(nPerm) 
-                            for perm = 1:nPerm 
-                                permVar = zeros(length(circularCord),nPerm);
-                                permVar(:,perm) = circularCord(randperm(length(circularCord)));
-                            end
-                            for perm = 1:nPerm
-                                [~,~,~,~,pgdPerm(timei,perm)] = circRegMod(permVar(:,perm),linearCord);
-                            end
-                        end
-                   end
-%               display(['Done with timepoint:',num2str(timei)])
-        end
-%%  get additional params
-    outputs.direction = direction+pi;
-    outputs.Wavelength = (1./sFreq).*elecDist;  % in m/rad
-    deltaT = 1/fs; % in s
-    phi_dot= abs(diff(circVmean))/deltaT; 
-    outputs.tempFreq = phi_dot/(2*pi); % convert to Hz
-    outputs.speed = outputs.tempFreq./(sFreq(2:end)/elecDist); % convert to m/s
-    outputs.pgd = pgd;
-    outputs.clusters = cluster; %the number of electrodes involved in the TW
-    outputs.mag = mag;
-    if ~isempty(nPerm)
-    outputs.pgdPerm = prctile(pgdPerm',0.99);
+if ~exist('nPerm','var');               nPerm = [];                     end
+
+% Parameters
+elecDist = 400*10^-6; %distance between adjacent electrodes in the array in m
+fs = 1/(timeVals(2)-timeVals(1)); %sampling frequency
+
+timePoints = length(timeVals);
+numGoodElectrodes = length(goodElectrodes);
+
+%initialize results
+pgd = zeros(timePoints,1);
+direction = zeros(timePoints,1);
+cluster = zeros(timePoints,1);
+circVmean = zeros(timePoints,1);
+sFreq = zeros(timePoints,1);
+coh = zeros(timePoints,1);
+
+burstMat(isnan(burstMat)) = 0;
+
+if ~isempty(nPerm)
+    pgdPerm = zeros(timePoints,nPerm);
+end
+
+numElectrodeCutoff = round(electrodeFraction*numel(goodElectrodes));
+
+% run circ reg after getting significant electrodes from burst detection
+for timei = 1:timePoints
+    phiGrid = phiMat(:,timei);
+    
+    if (numElectrodeCutoff==0) % take all electrodes
+        elecs = 1:numGoodElectrodes;
+    else
+        elecs = find(burstMat(:,timei));
     end
+    
+    if numel(elecs)>=numElectrodeCutoff
+        cluster(timei,1) = numel(elecs);
+        circularCord = phiGrid(elecs);
+        circVmean(timei,1) = circ_mean(circularCord);
+        coh(timei,1) = abs(mean(exp(1i*circularCord)));
+        linearCord = locList(elecs,:);
+
+        % do regression analysis on the polar and linear coordinates
+        [direction(timei,1),sFreq(timei,1),~,~,pgd(timei,1)] = circRegMod(circularCord,linearCord);
+        if ~isempty(nPerm)
+            for perm = 1:nPerm
+                permVar = zeros(length(circularCord),nPerm);
+                permVar(:,perm) = circularCord(randperm(length(circularCord)));
+            end
+            for perm = 1:nPerm
+                [~,~,~,~,pgdPerm(timei,perm)] = circRegMod(permVar(:,perm),linearCord);
+            end
+        end
+    end
+end
+
+%%  get additional params
+outputs.direction = direction+pi;
+outputs.Wavelength = (1./sFreq).*elecDist;  % in m/rad
+deltaT = 1/fs; % in s
+phi_dot= abs(diff(circVmean))/deltaT;
+outputs.tempFreq = phi_dot/(2*pi); % convert to Hz
+outputs.speed = outputs.tempFreq./(sFreq(2:end)/elecDist); % convert to m/s
+outputs.pgd = pgd;
+outputs.clusters = cluster; %the number of electrodes involved in the TW
+outputs.coh = coh;
+
+if ~isempty(nPerm)
+    outputs.pgdPerm = prctile(pgdPerm',0.99);
+end
 end
