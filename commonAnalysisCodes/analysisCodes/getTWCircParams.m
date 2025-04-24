0b6ae8ecaa6523bf0a6553fbee9d1a0e4eed11f1
gouthambhargava <42269270+gouthambhargava@users.noreply.github.com>
2025-04-24 10:00:05 +0530

    Added options for cluster based wave detection and for future EEG based changes

--- commonAnalysisCodes/analysisCodes/getTWCircParams.m
+++ commonAnalysisCodes/analysisCodes/getTWCircParams.m
@@ -1,37 +1,32 @@
-function outputs = getTWCircParams(phiMat,burstMat,timeVals,goodElectrodes,locList,electrodeFraction,electrodeChoice,nPerm)
+function outputs = getTWCircParams(phiMat,burstMat,timeVals,goodElectrodes,locList,electrodeFraction,electrodeChoice,arrayType,waveDetectionMethod,nPerm)
 % Inputs
 % phiMat - phases of single trial data in electrodes x time points format
 % burstMat - time series indicting burst times in electrodes x time points format
 % timeVals - vector of time values
 % goodElectrodes - list of good electrodes
-% locList - array of size electrodes x 2 containing the x and y position of each electrode
+% locList - array of electrodes x 2 containing the x and y position of each
+% for MEA. In case of EEG, supply the chanlocs structure.
+% electrode for microelectrodes or provide chanlocs file for EEG
 % electrodeFraction - identify as a cluster only when the number of
 % electrodes (out of all electrodes) showing a burst exceeds this fraction.
-% electrodeChoice - 'all' or 'selected': Choose either all electrodes or only the electrodes showing a burst for TW calculations 
+% electrodeChoice - 'all' or 'selected': Choose either all electrodes or only the electrodes showing a burst for TW calculations
 % nPerm - when not set to [], TW statistics are computed nPerm times after shuffling the phases.
+% arrayType - microelectrode or EEG - string
+% waveDetectionMethod - set to 1 for circlinear regression method. Set to 2
+% to apply cluster based circLinear regression method
 
 if ~exist('nPerm','var'); nPerm = [];  end
 
 % Parameters
-elecDist = 400*10^-6; %distance between adjacent electrodes in the array in m. Currently set to MEA by default. Option to select 
-                      %between EEG and MEA to be added in the future.
 fs = round(1/(timeVals(2)-timeVals(1))); %sampling frequency
 timePoints = length(timeVals);
-numGoodElectrodes = length(goodElectrodes);
-
-%initialize results
-pgd = zeros(timePoints,1);
-direction = nan(timePoints,1);
-cluster = zeros(timePoints,1);
-sFreq = zeros(timePoints,1);
-coh = zeros(timePoints,1);
+numGoodElectrodes = length(locList);
+neighbourLimit = 10;
 
 burstMat(isnan(burstMat)) = 0;
 burstVec = nansum(burstMat)/numGoodElectrodes;
 burstVec(burstVec>electrodeFraction) = 1;
 burstVec(burstVec~=1) = nan;
-% burstVec(burstVec>0) = 1;
-% burstVec(burstVec==0) = nan;
 
 if ~isempty(nPerm)
     pgdPerm = zeros(timePoints,nPerm);
@@ -39,39 +34,43 @@ end
 
 numElectrodeCutoff = round(electrodeFraction*numel(goodElectrodes));
 
-% run circ reg after getting significant electrodes from burst detection
+if strcmpi(arrayType,'Microelectrode')
+    elecDist = 400*10^-6; %distance between adjacent electrodes in the array in m.
+else
+    elecDist = 2; % set average interelectrode distance and reduce the dimensions of the electrode position data to facilitate circlinear regression (only done for 2d array)
+    locList = [locList(1:numGoodElectrodes).X;locList(1:numGoodElectrodes).Y;locList(1:numGoodElectrodes).Z]';
+    [coeff, score] = pca(locList);
+    locList = coeff(1:2,1:2)*score(:,1:2)'; % selects the highest two coefficients
+end
+
+%initialize results
+pgd = nan(numGoodElectrodes,timePoints);
+direction = nan(numGoodElectrodes,timePoints);
+% cluster = zeros(1,timePoints);
+sFreq = nan(numGoodElectrodes,timePoints);
+coh = zeros(timePoints,1);
+
+
+%% run circ reg after getting significant electrodes from burst detection
 for timei = 1:timePoints
     phiGrid = phiMat(:,timei);
-    
-    if strcmpi(electrodeChoice,'all') % take all electrodes
-        elecs = 1:numGoodElectrodes;
-    else
-        elecs = find(burstMat(:,timei));
-    end
-    
-    if numel(elecs)>=numElectrodeCutoff
-        cluster(timei,1) = numel(elecs);
-        circularCord = phiGrid(elecs);
-        coh(timei,1) = abs(mean(exp(1i*circularCord)));
-        linearCord = locList(elecs,:);
-        if numel(elecs)>3
-            % do regression analysis on the polar and linear coordinates
-            [direction(timei,1),sFreq(timei,1),~,~,pgd(timei,1)] = circRegMod(circularCord,linearCord);
-            if ~isempty(nPerm)
-                permVar = zeros(length(circularCord),nPerm);
-                for perm = 1:nPerm
-                    permVar(:,perm) = circularCord(randperm(length(circularCord)),:);
-                end
-                parfor perm = 1:nPerm
-                    [~,~,~,~,pgdPerm(timei,perm)] = circRegMod(permVar(:,perm),linearCord);
-                end
-            end
+    burstLocs = burstMat(:,timei);
+    % cluster(timei,1) = numel(elecs);
+    coh(timei,1) = abs(mean(exp(1i* phiGrid(burstLocs>0))));
+    [direction(:,timei), pgd(:,timei),sFreq(:,timei)] = getWaveMetrics(locList,phiGrid,burstLocs,waveDetectionMethod,neighbourLimit,electrodeChoice,numElectrodeCutoff);
+    % do regression analysis on the polar and linear coordinates
+    if ~isempty(nPerm)
+        permVar = zeros(length(phiGrid),nPerm);
+        for perm = 1:nPerm
+            permVar(:,perm) = phiGrid(randperm(length(circularCord)),:);
         end
-%          disp(['done with time ',num2str(timeVals(timei))])
+        for perm = 1:nPerm
+           [~, pgd(:,timei),~] = getWaveMetrics(locList,phaseMat,burstLocs,waveDetectionMethod,neighbourLimit,electrodeChoice,numElectrodeCutoff);
+        end
     end
 end
 
-% clean up the output variables. Removes values in PGD under 0 and also in
+%% clean up the output variables. Removes values in PGD under 0 and also in
 % direction, speed, sfreq and clusters
 pgd(pgd<0) = nan;
 direction(isnan(pgd)) = nan;
@@ -85,17 +84,18 @@ phi_dot = circ_mean(circ_dist(phiMat(:,2:end),phiMat(:,1:end-1)));
 tempFreq = (phi_dot/deltaT)/(2*pi); % convert to Hz
 tempFreq = [tempFreq,nan];
 tempFreq(tempFreq==0) = nan;
-outputs.speed = (tempFreq./sFreq')/1000; % convert to m/s
+outputs.speed = (tempFreq./sFreq)/1000; % convert to m/s
 outputs.tempFreq = wrapTo360(rad2deg(tempFreq));
 outputs.speed(outputs.speed==inf) = nan;
 outputs.pgd = pgd;
-outputs.clusters = cluster; %the number of electrodes involved in the TW
+% outputs.clusters = cluster; %the number of electrodes involved in the TW
 outputs.coh = coh;
 outputs.sFreq = sFreq;
 outputs.burstVec = burstVec;
+outputs.elecNum = size(locList,1);
 
 if ~isempty(nPerm)
-     outputs.pgdPerm = pgdPerm;
-     outputs.pgdPermP = prctile(pgdPerm',0.99);
+    outputs.pgdPerm = pgdPerm;
+    outputs.pgdPermP = prctile(pgdPerm',0.99);
 end
 end
---
+++ commonAnalysisCodes/analysisCodes/getWaveMetrics.m
@@ -0,0 +1,52 @@
+function [direction, pgd,spatialFreq] = getWaveMetrics(elecLocs,phaseData,burstLocs,waveMethod,neighbourLimit,electrodeChoice,numElectrodeCutoff)
+
+% Inputs
+% elecLocs is the list of electrode locations (x and y locations in the grid - dimensions reduced from 3D)
+% phaseData is the instant phases of all elecs at the time point
+% burstLocs is a logical vector, same length as the phaseData with gamma burst locations
+% neighbourLimit - specifies the number of neighbours to be considered for
+% calculating outputs for each electride
+% waveMethod - set to 1 - wave metrics by considering all electrodes in the array.
+%                     2 - wave metrics based on neighbouring clusters.
+% Outputs described in circRegressEEG.
+%% initialize the outputs
+elecLocs = reshape(elecLocs,length(elecLocs),2);
+numElectrodes = size(phaseData,1);
+if strcmpi(electrodeChoice,'all') %take all electrodes
+    burstElecs = 1:numGoodElectrodes;
+else
+    burstElecs = find(burstLocs);%find indices of electrodes that have bursts
+end
+
+
+if waveMethod ==1
+    direction = nan;
+    pgd = nan;
+    spatialFreq = nan;
+    if numel(burstElecs)>numElectrodeCutoff
+        [direction,spatialFreq,~,~,pgd] = circRegMod(phaseData(burstElecs),elecLocs(burstElecs,:)); % change circRegressEEG to circRegMod
+    end
+        direction = repmat(direction,numElectrodes,1);
+        spatialFreq = repmat(spatialFreq,numElectrodes,1);
+        pgd = repmat(pgd,numElectrodes,1);
+    
+else
+    direction = nan(numElectrodes,1);
+    pgd = nan(numElectrodes,1);
+    spatialFreq = nan(numElectrodes,1);
+    distmat = squareform(pdist(elecLocs)); % define the distance matrix
+    if numel(burstElecs)>numElectrodeCutoff
+        % select electrode clusters based on distance
+        for i = 1:length(elecLocs)
+            [~,sortedIndices] = sort(distmat(i,:)); % sort the electrodes based on distance from current electrode in the loop
+            neighbourElecs = sortedIndices(1:neighbourLimit);
+            sigElecs = intersect(neighbourElecs,burstElecs);
+            newLocs = elecLocs(sigElecs,:);
+            newPhases = phaseData(sigElecs,:);
+            if numel(newPhases)>2
+                [direction(i),spatialFreq(i),~,~,pgd(i)] = circRegMod(newPhases,newLocs);
+            end
+        end
+    end
+end
+end
